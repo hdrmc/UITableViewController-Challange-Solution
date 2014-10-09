434649f3443bbaf95fa369c1f3c7dc706c68a1d2
Pro Business Solutions IT <probs@MacBook-Pro-Pro.local>
2014-10-09 10:09:00 +0200

    Initial Commit

---
+++ UITableViewController Challange Solution/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITableViewController Challange Solution
+//
+//  Created by Pro Business Solutions IT on 09.10.2014.
+//  Copyright (c) 2014 Pro Business Solutions IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
